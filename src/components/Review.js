201a047eceaa940b2ed766154695b04216d42ab0
Satya Sai <satya07thota@gmail.com>
2023-11-12 12:47:52 +0530

    add

--- src/components/Review.js
+++ src/components/Review.js
@@ -11,7 +11,7 @@ const Review = () => {
         <div className="w-3/6 h-auto bg-gray-300 flex flex-col items-center p-4 mt-14 ">
           <div className="relative w-[150px] h-[150px] rounded-full mx-auto mb-6 bg-blue-400">
             <img src={image} alt="photo" className="w-full h-full block object-cover rounded-full relative m-2" />
-            <span className="relative top-0 left-0 to-white bg-blue-600 rounded-full">
+            <span className="relative top-0 left-0 to-white bg-blue-600 rounded-full n">
               <FaQuoteRight/>
             </span>
           </div>
